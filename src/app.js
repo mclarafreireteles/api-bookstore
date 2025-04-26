c567b9160a73cd0dcb651579862cb193219abee1
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-04-26 11:41:50 -0300

    :sparkles: Criando metodo POST

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@ import express from "express";
 
 const app = express()
 
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1,
+        título: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        título: "O Hobbit"
+    }
+];
+
 app.get("/", (req, res) => {
-    res.status(200).send("Estou enviando uma resposta!")
+    res.status(200).send("Estou enviando uma resposta!");
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!")
 })
 
 export default app
